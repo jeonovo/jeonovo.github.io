a09d2d3aba4318b2eb907864fb6fdfcf504c413d
jeonovo <jonnyjames.bland@gmail.com>
2015-05-26 20:36:28 +0100

    Site update
    Added panels

---
+++ js/panels.js
@@ -0,0 +1,9 @@
+$(document).ready(
+    function() {
+        $("#control1").click(function() {
+            $("#panel1").fadeIn();
+			$("#panel2").fadeOut();
+			$("#panel3").fadeOut();
+			$("#panel4").fadeOut();		
+    });
+});
