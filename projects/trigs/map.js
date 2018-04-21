324402944ae153d4fd129a460aa93689528f170a
jeonovo <jonnyjames.bland@gmail.com>
2018-04-21 11:49:25 +0100

    updated trigs to use tabletop

--- projects/trigs/map.js
+++ projects/trigs/map.js
@@ -1,57 +1,72 @@
 var markers;
 var cluster;
-
+var code = "1atk1vdHFDsJCt4J1HyxszwDM8Go_bmD_suajQzhnhwA";
 function initialize() {
 
   markers = [];
-	
 
-	var base =  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+	var base =   L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
 });
 
-	
-	// layers from https://github.com/leaflet-extras/leaflet-providers
 
+
+
+	// layers from https://github.com/leaflet-extras/leaflet-providers
 
 	var map = L.map('map', {
 		center: [54.9752342,-1.6512303],
     zoom: 10,
 		layers: base,
-		maxZoom: 18
+		maxZoom: 17
 	});
 
- geojson = L.geoJson(trigs)
- cluster = new L.MarkerClusterGroup(); 
+     cluster = new L.MarkerClusterGroup();
+
+    // loop through spreadsheet with Tabletop
+        Tabletop.init({
+            key: code,
+            callback: function(sheet, tabletop){
+
+                for (var id in sheet){
+                    // Info for the popup
+                    var info = "<html><div class=popup>"
+                    + "<div id=name>" + sheet[id].tp_name + "</div>";
+                  var markerLocation = new L.LatLng(sheet[id].latitude, sheet[id].longitude);
+
+                  // do a swicth statement for categories later
 
 
-  addMarkers();
-  map.addLayer(cluster); 
+                  // Create marker
+                  var  marker = new L.Marker(markerLocation).bindPopup(info,{maxWidth:150});
+                  // var  marker = new L.Marker(markerLocation, {icon: createIcon()}).bindPopup(info,{maxWidth:150});
 
-  function addMarkers(){
+                  markers.push(marker);
 
-    for (id in geojson._layers){
-  
-        // Info for the popup 
-        var info = "<html><div class=popup>" 
-        + "<div id=name>" + geojson._layers[id].feature.properties.tp_name + "</div>"; 
-      var markerLocation = new L.LatLng(geojson._layers[id].feature.properties.lat, geojson._layers[id].feature.properties.long);
-      
-      // do a swicth statement for categories later 
-      
-      
-      // Create marker
-      var  marker = new L.Marker(markerLocation, {icon: createIcon()}).bindPopup(info,{maxWidth:150}); 
-      
-      markers.push(marker); 
+                  cluster.addLayers(markers);
 
-      cluster.addLayers(markers); 
+                //map.addLayer(marker);
 
-    //map.addLayer(marker);
+                }
 
-    }
-  }
+
+              },
+            simpleSheet: true,
+        });
+
+
+// geojson = L.geoJson(trigs)
+
+
+
+  //addMarkers();
+  map.addLayer(cluster);
+
 
 function createIcon() {
 
@@ -59,7 +74,7 @@ function createIcon() {
         iconUrl: 'trigs/trigpoint1.png',
         iconSize:     [24, 24], // size of the icon
         iconAnchor:   [6, 12], // point of the icon which will correspond to marker's location
-        popupAnchor:  [6, -12] // point from which the popup should open relative to the iconAnchor
+        popupAnchor:  [7, -12] // point from which the popup should open relative to the iconAnchor
       });
       return icon;
 }
