927733ab8769e533fe51df2c15ecd88e2e72ee86
Jonny Bland <jonnyjames.bland@gmail.com>
2021-11-05 19:19:30 +0000

    Update map.js

--- projects/pubs/js/map.js
+++ projects/pubs/js/map.js
@@ -83,7 +83,7 @@ function initMap(){
 		    complete: function(results){
 			    sheet = results.data
 			    //console.log(sheet)    
-		    }})
+		    }
                 data = [];
                 if (typeof gj != "undefined"){
                     map.removeLayer(gj);
@@ -105,7 +105,7 @@ function initMap(){
 		console.log(data); 
 			setData(data);
                 
-    	}
+    	});
 	
 	
 	
